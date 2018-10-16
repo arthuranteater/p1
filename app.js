8313fb674bf55b70e4a7266c82ed9a704a578767
Hunt Applegate <huntapplegate@gmail.com>
2018-10-16 11:48:29 -0600

    1st proj added

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.parallax');
-//     var instances = M.Parallax.init(elems);
-//   });
-//
-// document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.scrollspy');
-//   var instances = M.ScrollSpy.init(elems);
-//  });
-//
-//  var instance = M.Carousel.init({
-//      fullWidth: true,
-//      indicators: true
-//    });
+document.addEventListener("DOMContentLoaded", function (event) {
+  M.AutoInit();
+
+  const email = document.querySelector("#email")
+  const form = document.querySelector('#form')
+  const control = document.querySelector("#control")
+  const carousel = document.querySelector(".carousel")
 
 
-M.AutoInit();
+  control.addEventListener("click", event => {
+    const instance = M.Carousel.getInstance(carousel)
+    console.log(event.target)
+    if (event.target.classList.contains("next")) {
+      instance.next()
+    } else if (event.target.classList.contains("prev")) {
+      instance.prev()
+    }
+  })
 
+  form.addEventListener("submit", event => {
+    if (email.value === '') {
+      event.preventDefault()
+      alert("Please add your email!")
+    } else {
+      alert("Thank you! We'll get back to you soon!")
+    }
+  })
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("DOM fully loaded and parsed");
 });
