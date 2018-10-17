5ecaabe80dc03451be0de36399f26f1d3792a4a6
Hunt Applegate <huntapplegate@gmail.com>
2018-10-17 16:32:03 -0600

    added post

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   M.AutoInit();
 
-  const email = document.querySelector("#email")
+  // const email = document.querySelector("#email")
   const form = document.querySelector('#form')
   const control = document.querySelector("#control")
   const carousel = document.querySelector(".carousel")
+
 
 
   control.addEventListener("click", event => {
@@ -18,12 +19,27 @@ document.addEventListener("DOMContentLoaded", function (event) {
   })
 
   form.addEventListener("submit", event => {
-    if (email.value === '') {
-      event.preventDefault()
-      alert("Please add your email!")
-    } else {
-      alert("Thank you! We'll get back to you soon!")
-    }
+    event.preventDefault()
+    const data = new FormData(form)
+    data.set('form-name', 'contact');
+    console.dir(data)
+    return fetch("/", {
+      method: "POST",
+      body: data
+    })
+      .then(response => {
+        if (response.status > 299) {
+          throw new Error("Failed to send message. Please try again.");
+        }
+        return response.text();
+      })
+      .then(data => {
+        console.log("Form submission success", data);
+        alert("Success!");
+      })
+      .catch(error => {
+        console.error("Form submission FAIL:", error);
+        alert('Message Failed');
+      })
   })
-
-});
+})
