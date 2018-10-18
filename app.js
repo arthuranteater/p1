d9cab850ae452b8041b5516d9c0322e259858de3
Hunt Applegate <huntapplegate@gmail.com>
2018-10-18 10:23:24 -0600

    added get

--- app.js
+++ app.js
@@ -21,37 +21,32 @@ document.addEventListener("DOMContentLoaded", function (event) {
   form.addEventListener("submit", event => {
     event.preventDefault()
     const data = new FormData(form)
-    data.set('form-name', 'contact');
-    console.dir(data)
+    data.set('form-name', 'contact')
+    // console.dir(data)
     fetch("/forms", {
       method: "POST",
       body: data
     })
       .then(response => {
         if (response.status > 299) {
-          throw new Error("Failed to send message. Please try again.");
+          throw new Error("Failed to send message. Please try again.")
         }
-        return response.text();
+        return response.text()
       })
       .then(data => {
-        console.log("Form submission success", data);
-        alert("Success!");
+        console.log("Form submission success", data)
+        alert("Success!")
+        fetch('/forms')
+          .then(function (response) {
+            return response.json()
+          })
+          .then(function (myJson) {
+            console.log(JSON.stringify(myJson))
+          })
       })
       .catch(error => {
-        console.error("Form submission FAIL:", error);
-        alert('Message Failed');
-      })
-
-    fetch('/forms')
-      .then(function (response) {
-        return response.json()
-      })
-      .then(function (myJson) {
-        console.log(JSON.stringify(myJson));
+        console.error("Form submission FAIL:", error)
+        alert('Message Failed')
       })
   })
-
-
-
-
 })
