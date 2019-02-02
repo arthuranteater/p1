de686700877286432336d5f7489f69df426c9152
Hunt Applegate <huntapplegate@gmail.com>
2019-02-02 04:33:31 -0700

    styled

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   const control = document.querySelector("#control")
   const carousel = document.querySelector(".carousel")
+  const logo = document.querySelector(".name")
+  const title = document.querySelector(".title")
+  const about = document.querySelector(".about")
+  const contact = document.querySelector(".contact")
 
+
+  logo.textContent = `<Hunt Applegate />`
+  title.textContent = `   Software Engineer`
+  about.textContent = "``` const dreams = ideas + failures + work^n ```"
+  contact.textContent = "``` { response = have a project in mind ? 'check a box' : 'say hi' } ```"
 
 
   control.addEventListener("click", event => {
     const instance = M.Carousel.getInstance(carousel)
-    console.log(event.target)
     if (event.target.classList.contains("next")) {
       instance.next()
     } else if (event.target.classList.contains("prev")) {
